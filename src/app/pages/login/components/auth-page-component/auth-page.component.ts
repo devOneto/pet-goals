a11350256515edcef29a39330fc73e28c143017c
Luis Carlos <luis.carlos@somapay.com.br>
2022-01-11 22:54:45 -0300

    Add components in login page

---
+++ src/app/pages/login/components/auth-page-component/auth-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-page',
+  templateUrl: './auth-page.component.html',
+  styleUrls: ['./auth-page.component.scss'],
+})
+export class AuthPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
