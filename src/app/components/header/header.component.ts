879cd462a6fca9eff7fe420c7b42d02b4f5ae2c4
(Neto)João Araújo <bipnerds@gmail.com>
2022-01-16 20:41:00 -0300

    Sair da Aplicação

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  storage: Storage;
 
-  ngOnInit() {}
+  constructor(
+    private router: Router
+  ) {
+    this.storage = window.sessionStorage;
+  }
+
+  ngOnInit() { }
+
+  exit() {
+    this.storage.clear();
+    this.router.navigateByUrl('')
+  }
 
 }
